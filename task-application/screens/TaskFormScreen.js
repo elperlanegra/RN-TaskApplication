8610216b016adf1ed5271d53a687436f5935d247
Manuel Duarte <carlosd99m@gmail.com>
2021-10-30 20:52:36 -0600

    feat: navegacione entre paginas

---
+++ task-application/screens/TaskFormScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TaskFormScreen = () => {
+  return (
+    <View>
+      <Text> TaskFormScreen </Text>
+    </View>
+  );
+};
+
+export default TaskFormScreen;
